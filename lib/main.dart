3257a5fee03356c88f94ff3af00612149aabea6f
Jerakin <the_rensvind@hotmail.com>
2023-10-11 22:31:44 +0200

    fix: cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Super Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
@@ -211,7 +196,6 @@ class _TimerScreenState extends State<TimerScreen> {
         ],
       ),
     );
-
   }
 }
 
@@ -229,7 +213,6 @@ EdgeInsets playerButtonEdge(thisPlayer) {
 BoxDecoration playerButtonDecoration(thisPlayer, currentPlayer) {
   Color colorActive = Colors.white24;
   Color colorInactive = Colors.black12;
-
 
   return BoxDecoration(
     color: thisPlayer == currentPlayer ? colorActive : colorInactive,
